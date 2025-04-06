fb0263b283cb6f3b574e99ea4659cbe7214a63d4
Savalige <matsbennervall@gmail.com>
2025-04-06 18:01:37 +0200

    Radar enemy positions

--- godot/scenes/gui/radar_gui.gd
+++ godot/scenes/gui/radar_gui.gd
@@ -1,11 +1,8 @@
 extends Control
 
 @onready var timer: Timer = $Timer
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
+var color: Color = Color.RED
+var alpha = 0
 
 func _draw():
 	for x in range(0, 10):
@@ -14,9 +11,18 @@ func _draw():
 		draw_line(Vector2(0,y*50+25), Vector2(500, y*50+25), Color.WHITE)
 	
 	if not timer.is_stopped():
+		ScrGlobalRts.emit_attention(Vector2(500, 500), 200)
+		var radius = ((timer.wait_time - timer.time_left) / timer.wait_time) * 500
 		draw_arc(Vector2(250, 250), 
-		((timer.wait_time - timer.time_left) / timer.wait_time) * 500, 
+		radius, 
 		0, PI*2, 100, Color.PURPLE)
+		
+		for e in ScrGlobalRts.monsters:
+			var distance = (e.pos/2).distance_to(Vector2(250, 250))
+			if distance < radius + 0.5 and distance > radius - 0.5:
+				alpha = 1
+			color.a = alpha
+			draw_circle(e.pos/2, 10, color)
 	
 	for d in ScrGlobalRts.drones:
 		draw_circle(d.pos/2, 10, Color.BLUE)
@@ -25,7 +31,11 @@ func _draw():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func _process(delta):
+	if alpha > 0:
+		alpha -= 1 * delta
+	else:
+		alpha = 0
 	queue_redraw()
 
 
